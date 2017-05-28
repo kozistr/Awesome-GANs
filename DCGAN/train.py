2d17acfa192cae7fb882d6027793882c332b63e7
kozistr <kozistr@gmail.com>
2017-05-29 03:13:20 +0900

    change comment about taking time

--- DCGAN/train.py
+++ DCGAN/train.py
@@ -22,7 +22,7 @@ dirs = {
     'model': './model/LAPGAN-model.ckpt'
 }
 paras = {
-    'epoch': 200,  # with GTX 1080 11gb, takes 5750s
+    'epoch': 200,  # with GTX 1080 11gb, takes 4600s
     'batch_size': 64,
     'logging_interval': 750
 }
