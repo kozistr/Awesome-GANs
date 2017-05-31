d9a498e710eca7d5eb04c881b020901ac41c5e48
kozistr <kozistr@gmail.com>
2017-05-31 22:56:28 +0900

    add loss function & summary

--- BEGAN/began.py
+++ BEGAN/began.py
@@ -50,6 +50,10 @@ def resize_nn(x, size):
     return tf.image.resize_nearest_neighbor(x, size=(int(size), int(size)))
 
 
+def l1_loss(x, y):
+    return tf.reduce_mean(tf.abs(x - y))
+
+
 class BEGAN:
 
     def __init__(self, s, input_height=64, input_width=64, output_height=64, output_width=64, channel=3,
@@ -87,14 +91,6 @@ class BEGAN:
         self.g_lr_update = tf.assign(self.g_lr, tf.maximum(self.g_lr * 0.5, lr_low_boundary, name="g_lr_update"))
         self.d_lr_update = tf.assign(self.d_lr, tf.maximum(self.d_lr * 0.5, lr_low_boundary, name="d_lr_update"))
 
-        self.learning_rate = tf.train.exponential_decay(
-            learning_rate=1e-4,
-            decay_rate=0.95,
-            decay_steps=2000,
-            global_step=2e5,  # 200k
-            staircase=False
-        )
-
         self.build_began()
 
     def encoder(self, x, embedding, reuse=None):
@@ -114,7 +110,7 @@ class BEGAN:
                         x = slim.conv2d(x, channel_num, kernel_size=3, stride=2)  # sub-sampling
                         # x = slim.max_pooling2d(x, 3, 2)
 
-                x = tf.reshape(x, [-1, np.prod([8, 8, channel_num]))
+                x = tf.reshape(x, [-1, np.prod([8, 8, channel_num])])
         return x
 
     def decoder(self, z, embedding, reuse=None):
@@ -138,10 +134,10 @@ class BEGAN:
 
     def discriminator(self, x, embedding, reuse=None):
         with tf.variable_scope("discriminator", reuse=reuse):
-            x = self.encoder(x, embedding, reuse=reuse)
-            x = self.decoder(x, embedding, reuse=reuse)
+            z = self.encoder(x, embedding, reuse=reuse)
+            x = self.decoder(z, embedding, reuse=reuse)
 
-            return x
+            return x, z
 
     def generator(self, z, embedding, reuse=None):
         with tf.variable_scope("generator/decoder", reuse=reuse):
@@ -150,17 +146,59 @@ class BEGAN:
             return x
 
     def build_began(self):
-        self.x = tf.placeholder(tf.float32, [self.batch_size, self.input_width], "x-image")
-        self.y = tf.placeholder(tf.float32, [self.batch_size, self.sample_size, self.sample_size, self.channel])
+        # x, z placeholder
+        self.x = tf.placeholder(tf.float32, shape=[self.batch_size] + self.image_shape, name='x-images')
+        self.z = tf.placeholder(tf.float32, shape=[self.batch_size, self.embedding], name='z-noise')
 
         self.lr = tf.placeholder(tf.float32, "learning-rate")
-        self.kt = tf.placeholder(tf.float32, "kt")
+        self.kt = tf.placeholder(tf.float32, "k_t")
 
         # Generator Model
-        self.G = self.generator(self.x)
+        self.G = self.generator(self.z)
 
-        # Discriminator Model
-        self.D = self.decoder(self.x, reuse=True)
-        self.D_real = self.discriminator(self.y)
-        self.D_fake = self.discriminator(self.G, reuse=True)
+        # Discriminator Model (Critic)
+        self.D_real = self.discriminator(self.x, self.embedding)  # discriminator
+        self.D_fake = self.discriminator(self.G, self.embedding, reuse=True)  # discriminate
 
+        # Generator & Discriminator loss(l1 loss)
+        # loss of D = loss of D_real - kt * loss of D_fake (0 < k_t < 1, k_0 = 0)
+        # loss of G = loss of D_fake
+        self.D_real_loss = l1_loss(self.x, self.D_real)
+        self.D_fake_loss = l1_loss(self.G, self.D_fake)
+        self.D_loss = self.D_real_loss - self.kt * self.D_fake_loss
+
+        self.G_loss = self.D_fake_loss
+
+        self.M_global = self.D_real_loss + (self.gamma * self.D_real_loss * self.D_fake_loss)
+
+        # summary
+        self.z_sum = tf.summary.histogram("z", self.z)
+
+        self.G_sum = tf.summary.image("G", self.G)  # generated image from G model
+        self.D_real_sum = tf.summary.histogram("D_real", self.D_real)
+        self.D_fake_sum = tf.summary.histogram("D_fake", self.D_fake)
+
+        self.d_real_loss_sum = tf.summary.scalar("d_real_loss", self.D_real_loss)
+        self.d_fake_loss_sum = tf.summary.scalar("d_fake_loss", self.D_fake_loss)
+        self.d_loss_sum = tf.summary.scalar("d_loss", self.D_loss)
+        self.g_loss_sum = tf.summary.scalar("g_loss", self.G_loss)
+
+        # collect trainer values
+        vars = tf.trainable_variables()
+        self.d_vars = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, "discriminator")
+        self.g_vars = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, "generator")
+
+        # model saver
+        self.saver = tf.train.Saver()
+
+        # optimizer
+        self.d_op = tf.train.AdamOptimizer(learning_rate=self.d_lr_update, beta1=self.mm1).\
+            minimize(self.D_loss, var_list=self.d_vars)
+        self.g_op = tf.train.AdamOptimizer(learning_rate=self.g_lr_update, beta1=self.mm1).\
+            minimize(self.G_loss, var_list=self.g_vars)
+
+        # merge summary
+        self.g_sum = tf.summary.merge([self.z_sum, self.D_fake_sum, self.G_sum, self.d_fake_loss_sum, self.g_loss_sum])
+        self.d_sum = tf.summary.merge([self.z_sum, self.D_real_sum, self.d_real_loss_sum, self.d_loss_sum])
+        self.merged = tf.summary.merge_all()
+        self.writer = tf.summary.FileWriter('./model/', self.s.graph)
