c85315ed0f0aa1e93bedfca28c76b7a663a6f45b
kozistr <kozistr@gmail.com>
2017-06-03 22:47:23 +0900

    change relu to maxout and some stuff changes...

--- CGAN/cgan.py
+++ CGAN/cgan.py
@@ -8,8 +8,8 @@ class CGAN:
 
     def __init__(self, s, batch_size=64,
                  input_height=28, input_width=28, channel=1, z_dim=100, sample_num=64, sample_size=8,
-                 output_height=28, output_width=28, n_input=784, n_classes=10,
-                 n_hidden_layer_1=128, n_hidden_layer_2=128, g_lr=1e-3, d_lr=1e-3, epsilon=1e-12):
+                 output_height=28, output_width=28, n_input=784, n_classes=10, maxout_unit=8,
+                 n_hidden_layer_1=128, n_hidden_layer_2=256, epsilon=1e-9):
         self.s = s
         self.batch_size = batch_size
 
@@ -27,31 +27,37 @@ class CGAN:
         self.n_hl_1 = n_hidden_layer_1
         self.n_hl_2 = n_hidden_layer_2
         self.n_classes = n_classes
+        self.maxout_unit = maxout_unit
 
         self.eps = epsilon
-
-        self.d_lr, self.g_lr = d_lr, g_lr
 
         self.build_cgan()
 
     def discriminator(self, x, y, reuse=None):  # simple neural networks
         with tf.variable_scope("discriminator", reuse=reuse):
             inputs = tf.concat(axis=1, values=[x, y])
+
             net = tf.nn.bias_add(tf.matmul(inputs, self.W['d_h1']), self.b['d_b1'])
-            net = tf.nn.relu(net)
-            net = tf.nn.dropout(net, 0.8)
+            net = tf.reshape(net, [-1, self.maxout_unit, self.n_hl_1])
+
+            net = tf.reduce_max(net, reduction_indices=[1])
+            net = tf.nn.dropout(net, 0.5)
 
             net = tf.nn.bias_add(tf.matmul(net, self.W['d_h2']), self.b['d_b2'])
-            net = tf.nn.relu(net)
-            net = tf.nn.dropout(net, 0.8 - 0.05)
+            net = tf.reshape(net, [-1, self.maxout_unit, self.n_hl_2])
+
+            net = tf.reduce_max(net, reduction_indices=[1])
+            net = tf.nn.dropout(net, 0.5)
 
             logits = tf.nn.bias_add(tf.matmul(net, self.W['d_h_out']), self.b['d_b_out'])
+            prob = tf.nn.sigmoid(logits)
 
-        return logits
+        return prob
 
     def generator(self, z, y, reuse=None):
         with tf.variable_scope("generator", reuse=reuse):
             inputs = tf.concat(axis=1, values=[z, y])
+
             de_net = tf.nn.bias_add(tf.matmul(inputs, self.W['g_h1']), self.b['g_b1'])
             de_net = tf.nn.relu(de_net)
 
@@ -59,8 +65,9 @@ class CGAN:
             de_net = tf.nn.relu(de_net)
 
             logits = tf.nn.bias_add(tf.matmul(de_net, self.W['g_h_out']), self.b['g_b_out'])
+            prob = tf.nn.tanh(logits)
 
-        return logits
+        return prob
 
     def build_cgan(self):
         # x, c, z placeholder
@@ -71,9 +78,9 @@ class CGAN:
         # weights
         self.W = {
             # weights for discriminator
-            'd_h1': tf.get_variable('d_h1', shape=[self.n_input + self.n_classes, self.n_hl_1],
+            'd_h1': tf.get_variable('d_h1', shape=[self.n_input + self.n_classes, self.maxout_unit * self.n_hl_1],
                                     initializer=tf.contrib.layers.variance_scaling_initializer()),
-            'd_h2': tf.get_variable('d_h2', shape=[self.n_hl_1, self.n_hl_2],
+            'd_h2': tf.get_variable('d_h2', shape=[self.n_hl_1, self.maxout_unit * self.n_hl_2],
                                     initializer=tf.contrib.layers.variance_scaling_initializer()),
             'd_h_out': tf.get_variable('d_h_out', shape=[self.n_hl_2, 1],
                                        initializer=tf.contrib.layers.variance_scaling_initializer()),
@@ -89,8 +96,8 @@ class CGAN:
         # bias
         self.b = {
             # biases for discriminator
-            'd_b1': tf.Variable(tf.zeros([self.n_hl_1])),
-            'd_b2': tf.Variable(tf.zeros([self.n_hl_2])),
+            'd_b1': tf.Variable(tf.zeros([self.maxout_unit * self.n_hl_1])),
+            'd_b2': tf.Variable(tf.zeros([self.maxout_unit * self.n_hl_2])),
             'd_b_out': tf.Variable(tf.zeros([1])),
             # biases for generator
             'g_b1': tf.Variable(tf.zeros([self.n_hl_2])),
@@ -105,16 +112,12 @@ class CGAN:
         self.D_real = self.discriminator(self.x, self.c)
         self.D_fake = self.discriminator(self.G, self.c, reuse=True)
 
-        # loss function with sigmoid
-        self.g_loss = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(
-            logits=self.D_fake, labels=tf.ones_like(self.D_fake)))
+        # maximize log(D(G(z)))
+        self.g_loss = -tf.reduce_mean(tf.log(self.D_fake + self.eps))
 
-        self.d_real_loss = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(
-            logits=self.D_real, labels=tf.ones_like(self.D_real)
-        ))
-        self.d_fake_loss = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(
-            logits=self.D_fake, labels=tf.zeros_like(self.D_fake)
-        ))
+        # maximize log(D(x)) + log(1 - D(G(z)))
+        self.d_real_loss = -tf.reduce_mean(tf.log(self.D_real + self.eps))
+        self.d_fake_loss = -tf.reduce_mean(tf.log((1. - self.D_fake) + self.eps))
         self.d_loss = self.d_real_loss + self.d_fake_loss
 
         # summary
