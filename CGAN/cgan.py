144255b7acef6b66152ab1715e038e62bfbb291b
kozistr <kozistr@gmail.com>
2017-06-03 20:34:19 +0900

    edit loss function

--- CGAN/cgan.py
+++ CGAN/cgan.py
@@ -22,7 +22,6 @@ class CGAN:
         self.output_width = output_width
         self.channel = channel
         self.z_dim = z_dim
-        self.y_dim = n_classes
 
         self.n_input = n_input
         self.n_hl_1 = n_hidden_layer_1
@@ -68,20 +67,20 @@ class CGAN:
     def build_cgan(self):
         # x, c, z placeholder
         self.x = tf.placeholder(tf.float32, shape=[None, self.n_input], name="x-image")
-        self.c = tf.placeholder(tf.float32, shape=[None, self.y_dim], name='c-condition')
+        self.c = tf.placeholder(tf.float32, shape=[None, self.n_classes], name='c-condition')
         self.z = tf.placeholder(tf.float32, shape=[None, self.z_dim], name='z-noise')
 
         # weights
         self.W = {
             # weights for discriminator
-            'd_h1': tf.get_variable('d_h1', shape=[self.n_input + self.y_dim, self.n_hl_1],
+            'd_h1': tf.get_variable('d_h1', shape=[self.n_input + self.n_classes, self.n_hl_1],
                                     initializer=tf.contrib.layers.variance_scaling_initializer()),
             'd_h2': tf.get_variable('d_h2', shape=[self.n_hl_1, self.n_hl_2],
                                     initializer=tf.contrib.layers.variance_scaling_initializer()),
             'd_h_out': tf.get_variable('d_h_out', shape=[self.n_hl_2, 1],
                                        initializer=tf.contrib.layers.variance_scaling_initializer()),
             # weights for generator
-            'g_h1': tf.get_variable('g_h1', shape=[self.z_dim + self.y_dim, self.n_hl_2],
+            'g_h1': tf.get_variable('g_h1', shape=[self.z_dim + self.n_classes, self.n_hl_2],
                                     initializer=tf.contrib.layers.variance_scaling_initializer()),
             'g_h2': tf.get_variable('g_h2', shape=[self.n_hl_2, self.n_hl_1],
                                     initializer=tf.contrib.layers.variance_scaling_initializer()),
@@ -108,12 +107,16 @@ class CGAN:
         self.D_real = self.discriminator(self.x, self.c)
         self.D_fake = self.discriminator(self.G, self.c, reuse=True)
 
-        # maximize log(D(G(z)))
-        self.g_loss = -tf.reduce_mean(tf.log(self.D_fake + self.eps))
+        # loss function with sigmoid
+        self.g_loss = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(
+            logits=self.D_fake, labels=tf.ones_like(self.D_fake)))
 
-        # maximize log(D(x)) + log(1 - D(G(z)))
-        self.d_real_loss = -tf.reduce_mean(tf.log(self.D_real + self.eps))
-        self.d_fake_loss = -tf.reduce_mean(tf.log((1. - self.D_fake) + self.eps))
+        self.d_real_loss = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(
+            logits=self.D_real, labels=tf.ones_like(self.D_real)
+        ))
+        self.d_fake_loss = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(
+            logits=self.D_fake, labels=tf.zeros_like(self.D_fake)
+        ))
         self.d_loss = self.d_real_loss + self.d_fake_loss
 
         # summary
