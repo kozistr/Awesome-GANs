0c8a8abb764a895b9308c637a2404737aeeda377
kozistr <kozistr@gmail.com>
2017-06-03 23:09:25 +0900

    remove second hidden layer

--- CGAN/cgan.py
+++ CGAN/cgan.py
@@ -9,7 +9,7 @@ class CGAN:
     def __init__(self, s, batch_size=64,
                  input_height=28, input_width=28, channel=1, z_dim=100, sample_num=64, sample_size=8,
                  output_height=28, output_width=28, n_input=784, n_classes=10, maxout_unit=8,
-                 n_hidden_layer_1=128, n_hidden_layer_2=256, epsilon=1e-9):
+                 n_hidden_layer_1=128, epsilon=1e-9):
         self.s = s
         self.batch_size = batch_size
 
@@ -25,7 +25,6 @@ class CGAN:
 
         self.n_input = n_input
         self.n_hl_1 = n_hidden_layer_1
-        self.n_hl_2 = n_hidden_layer_2
         self.n_classes = n_classes
         self.maxout_unit = maxout_unit
 
@@ -43,12 +42,6 @@ class CGAN:
             net = tf.reduce_max(net, reduction_indices=[1])
             net = tf.nn.dropout(net, 0.5)
 
-            net = tf.nn.bias_add(tf.matmul(net, self.W['d_h2']), self.b['d_b2'])
-            net = tf.reshape(net, [-1, self.maxout_unit, self.n_hl_2])
-
-            net = tf.reduce_max(net, reduction_indices=[1])
-            net = tf.nn.dropout(net, 0.5)
-
             logits = tf.nn.bias_add(tf.matmul(net, self.W['d_h_out']), self.b['d_b_out'])
             prob = tf.nn.sigmoid(logits)
 
@@ -61,11 +54,8 @@ class CGAN:
             de_net = tf.nn.bias_add(tf.matmul(inputs, self.W['g_h1']), self.b['g_b1'])
             de_net = tf.nn.relu(de_net)
 
-            de_net = tf.nn.bias_add(tf.matmul(de_net, self.W['g_h2']), self.b['g_b2'])
-            de_net = tf.nn.relu(de_net)
-
             logits = tf.nn.bias_add(tf.matmul(de_net, self.W['g_h_out']), self.b['g_b_out'])
-            prob = tf.nn.tanh(logits)
+            prob = tf.nn.sigmoid(logits)
 
         return prob
 
@@ -80,14 +70,10 @@ class CGAN:
             # weights for discriminator
             'd_h1': tf.get_variable('d_h1', shape=[self.n_input + self.n_classes, self.maxout_unit * self.n_hl_1],
                                     initializer=tf.contrib.layers.variance_scaling_initializer()),
-            'd_h2': tf.get_variable('d_h2', shape=[self.n_hl_1, self.maxout_unit * self.n_hl_2],
-                                    initializer=tf.contrib.layers.variance_scaling_initializer()),
-            'd_h_out': tf.get_variable('d_h_out', shape=[self.n_hl_2, 1],
+            'd_h_out': tf.get_variable('d_h_out', shape=[self.n_hl_1, 1],
                                        initializer=tf.contrib.layers.variance_scaling_initializer()),
             # weights for generator
-            'g_h1': tf.get_variable('g_h1', shape=[self.z_dim + self.n_classes, self.n_hl_2],
-                                    initializer=tf.contrib.layers.variance_scaling_initializer()),
-            'g_h2': tf.get_variable('g_h2', shape=[self.n_hl_2, self.n_hl_1],
+            'g_h1': tf.get_variable('g_h1', shape=[self.z_dim + self.n_classes, self.n_hl_1],
                                     initializer=tf.contrib.layers.variance_scaling_initializer()),
             'g_h_out': tf.get_variable('g_h_out', shape=[self.n_hl_1, self.n_input],
                                        initializer=tf.contrib.layers.variance_scaling_initializer()),
@@ -97,11 +83,9 @@ class CGAN:
         self.b = {
             # biases for discriminator
             'd_b1': tf.Variable(tf.zeros([self.maxout_unit * self.n_hl_1])),
-            'd_b2': tf.Variable(tf.zeros([self.maxout_unit * self.n_hl_2])),
             'd_b_out': tf.Variable(tf.zeros([1])),
             # biases for generator
-            'g_b1': tf.Variable(tf.zeros([self.n_hl_2])),
-            'g_b2': tf.Variable(tf.zeros([self.n_hl_1])),
+            'g_b1': tf.Variable(tf.zeros([self.n_hl_1])),
             'g_b_out': tf.Variable(tf.zeros([self.n_input])),
         }
 
@@ -139,11 +123,11 @@ class CGAN:
 
         # optimizer
         self.d_op = tf.train.AdamOptimizer().\
-            minimize(self.d_loss, var_list=[self.W['d_h1'], self.W['d_h2'], self.W['d_h_out'],
-                                            self.b['d_b1'], self.b['d_b2'], self.b['d_b_out']])
+            minimize(self.d_loss, var_list=[self.W['d_h1'], self.W['d_h_out'],
+                                            self.b['d_b1'], self.b['d_b_out']])
         self.g_op = tf.train.AdamOptimizer().\
-            minimize(self.g_loss, var_list=[self.W['g_h1'], self.W['g_h2'], self.W['g_h_out'],
-                                            self.b['g_b1'], self.b['g_b2'], self.b['g_b_out']])
+            minimize(self.g_loss, var_list=[self.W['g_h1'], self.W['g_h_out'],
+                                            self.b['g_b1'], self.b['g_b_out']])
 
         # merge summary
         self.g_sum = tf.summary.merge([self.z_sum, self.c_sum, self.D_fake_sum, self.G_sum, self.d_fake_loss_sum, self.g_loss_sum])
