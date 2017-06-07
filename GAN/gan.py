7950a60747d4fd4e3eec5c75966c84f76281a176
kozistr <kozistr@gmail.com>
2017-06-07 17:10:27 +0900

    add one more loss function and change activation function - softmax classification loss fn - to maxout+dropout - remove one neural network layer

--- GAN/gan.py
+++ GAN/gan.py
@@ -6,10 +6,10 @@ tf.set_random_seed(777)
 
 class GAN:
 
-    def __init__(self, s, batch_size=64,
+    def __init__(self, s, batch_size=32,
                  input_height=28, input_width=28, channel=1, z_dim=100, sample_num=64, sample_size=8,
-                 output_height=28, output_width=28, n_input=784, n_classes=10,
-                 n_hidden_layer_1=64, n_hidden_layer_2=128, g_lr=1e-3, d_lr=1e-3, epsilon=1e-8):
+                 output_height=28, output_width=28, n_input=784, n_classes=10, maxout_unit=8,
+                 n_hidden_layer_1=128, g_lr=2e-4, d_lr=2e-4, epsilon=1e-9):
         self.s = s
         self.batch_size = batch_size
 
@@ -23,9 +23,10 @@ class GAN:
         self.channel = channel
         self.z_dim = z_dim
 
+        self.maxout_unit = maxout_unit
+
         self.n_input = n_input
         self.n_hl_1 = n_hidden_layer_1
-        self.n_hl_2 = n_hidden_layer_2
         self.n_classes = n_classes
 
         self.eps = epsilon
@@ -37,29 +38,22 @@ class GAN:
     def discriminator(self, x, reuse=None):  # simple neural networks
         with tf.variable_scope("discriminator", reuse=reuse):
             net = tf.nn.bias_add(tf.matmul(x, self.W['d_h1']), self.b['d_b1'])
-            net = tf.nn.relu(net)
-            net = tf.nn.dropout(net, 0.8)
+            net = tf.reshape(net, [-1, self.maxout_unit, self.n_hl_1])
 
-            net = tf.nn.bias_add(tf.matmul(net, self.W['d_h2']), self.b['d_b2'])
-            net = tf.nn.relu(net)
-            net = tf.nn.dropout(net, 0.8 - 0.05)
+            net = tf.reduce_max(net, reduction_indices=[1])
+            net = tf.nn.dropout(net, 0.5)
 
             logits = tf.nn.bias_add(tf.matmul(net, self.W['d_h_out']), self.b['d_b_out'])
-            prob = tf.nn.sigmoid(logits)
-
-        return prob
+            # prob = tf.nn.sigmoid(logits)
+        return logits
 
     def generator(self, z, reuse=None):
         with tf.variable_scope("generator", reuse=reuse):
             de_net = tf.nn.bias_add(tf.matmul(z, self.W['g_h1']), self.b['g_b1'])
             de_net = tf.nn.relu(de_net)
 
-            de_net = tf.nn.bias_add(tf.matmul(de_net, self.W['g_h2']), self.b['g_b2'])
-            de_net = tf.nn.relu(de_net)
-
             logits = tf.nn.bias_add(tf.matmul(de_net, self.W['g_h_out']), self.b['g_b_out'])
-            prob = tf.nn.tanh(logits)
-
+            prob = tf.nn.sigmoid(logits)
         return prob
 
     def build_gan(self):
@@ -70,16 +64,12 @@ class GAN:
         # weights
         self.W = {
             # weights for discriminator
-            'd_h1': tf.get_variable('d_h1', shape=[self.n_input, self.n_hl_1],
+            'd_h1': tf.get_variable('d_h1', shape=[self.n_input, self.maxout_unit * self.n_hl_1],
                                     initializer=tf.contrib.layers.variance_scaling_initializer()),
-            'd_h2': tf.get_variable('d_h2', shape=[self.n_hl_1, self.n_hl_2],
-                                    initializer=tf.contrib.layers.variance_scaling_initializer()),
-            'd_h_out': tf.get_variable('d_h_out', shape=[self.n_hl_2, 1],
+            'd_h_out': tf.get_variable('d_h_out', shape=[self.n_hl_1, 1],
                                        initializer=tf.contrib.layers.variance_scaling_initializer()),
             # weights for generator
-            'g_h1': tf.get_variable('g_h1', shape=[self.z_dim, self.n_hl_2],
-                                    initializer=tf.contrib.layers.variance_scaling_initializer()),
-            'g_h2': tf.get_variable('g_h2', shape=[self.n_hl_2, self.n_hl_1],
+            'g_h1': tf.get_variable('g_h1', shape=[self.z_dim, self.n_hl_1],
                                     initializer=tf.contrib.layers.variance_scaling_initializer()),
             'g_h_out': tf.get_variable('g_h_out', shape=[self.n_hl_1, self.n_input],
                                        initializer=tf.contrib.layers.variance_scaling_initializer()),
@@ -88,12 +78,10 @@ class GAN:
         # bias
         self.b = {
             # biases for discriminator
-            'd_b1': tf.Variable(tf.zeros([self.n_hl_1])),
-            'd_b2': tf.Variable(tf.zeros([self.n_hl_2])),
+            'd_b1': tf.Variable(tf.zeros([self.maxout_unit * self.n_hl_1])),
             'd_b_out': tf.Variable(tf.zeros([1])),
             # biases for generator
-            'g_b1': tf.Variable(tf.zeros([self.n_hl_2])),
-            'g_b2': tf.Variable(tf.zeros([self.n_hl_1])),
+            'g_b1': tf.Variable(tf.zeros([self.n_hl_1])),
             'g_b_out': tf.Variable(tf.zeros([self.n_input])),
         }
 
@@ -104,6 +92,7 @@ class GAN:
         self.D_real = self.discriminator(self.x)
         self.D_fake = self.discriminator(self.G, reuse=True)
 
+        '''
         # maximize log(D(G(z)))
         self.g_loss = -tf.reduce_mean(tf.log(self.D_fake + self.eps))
 
@@ -111,6 +100,19 @@ class GAN:
         self.d_real_loss = -tf.reduce_mean(tf.log(self.D_real + self.eps))
         self.d_fake_loss = -tf.reduce_mean(tf.log((1. - self.D_fake) + self.eps))
         self.d_loss = self.d_real_loss + self.d_fake_loss
+        '''
+        # softmax loss
+        # Z_B = sigma x∈B exp(−μ(x)), −μ(x) is discriminator
+        Z_B = tf.reduce_sum(tf.exp(-self.D_real)) + tf.reduce_sum(tf.exp(-self.D_fake))
+
+        self.B_plus = self.batch_size
+        self.B_minus = self.batch_size * 2
+
+        # L_G = sigma x∈B+ μ(x)/abs(B) + sigma x∈B- μ(x)/abs(B) + ln(Z_B), B+ : batch _size
+        self.g_loss = tf.reduce_sum(self.D_real / self.B_plus) + tf.reduce_sum(self.D_fake / self.B_minus) + tf.log(Z_B)
+
+        # L_D = sigma x∈B+ μ(x)/abs(B) + ln(Z_B), B+ : batch _size
+        self.d_loss = tf.reduce_sum(self.D_real / self.B_plus) + tf.log(Z_B)
 
         # summary
         self.z_sum = tf.summary.histogram("z", self.z)
@@ -120,8 +122,8 @@ class GAN:
         self.D_real_sum = tf.summary.histogram("D_real", self.D_real)
         self.D_fake_sum = tf.summary.histogram("D_fake", self.D_fake)
 
-        self.d_real_loss_sum = tf.summary.scalar("d_real_loss", self.d_real_loss)
-        self.d_fake_loss_sum = tf.summary.scalar("d_fake_loss", self.d_fake_loss)
+        # self.d_real_loss_sum = tf.summary.scalar("d_real_loss", self.d_real_loss)
+        # self.d_fake_loss_sum = tf.summary.scalar("d_fake_loss", self.d_fake_loss)
         self.d_loss_sum = tf.summary.scalar("d_loss", self.d_loss)
         self.g_loss_sum = tf.summary.scalar("g_loss", self.g_loss)
 
@@ -130,14 +132,14 @@ class GAN:
 
         # optimizer
         self.d_op = tf.train.AdamOptimizer(learning_rate=self.d_lr).\
-            minimize(self.d_loss, var_list=[self.W['d_h1'], self.W['d_h2'], self.W['d_h_out'],
-                                            self.b['d_b1'], self.b['d_b2'], self.b['d_b_out']])
+            minimize(self.d_loss, var_list=[self.W['d_h1'], self.W['d_h_out'],
+                                            self.b['d_b1'], self.b['d_b_out']])
         self.g_op = tf.train.AdamOptimizer(learning_rate=self.g_lr).\
-            minimize(self.g_loss, var_list=[self.W['g_h1'], self.W['g_h2'], self.W['g_h_out'],
-                                            self.b['g_b1'], self.b['g_b2'], self.b['g_b_out']])
+            minimize(self.g_loss, var_list=[self.W['g_h1'], self.W['g_h_out'],
+                                            self.b['g_b1'], self.b['g_b_out']])
 
         # merge summary
-        self.g_sum = tf.summary.merge([self.z_sum, self.D_fake_sum, self.G_sum, self.d_fake_loss_sum, self.g_loss_sum])
-        self.d_sum = tf.summary.merge([self.z_sum, self.D_real_sum, self.d_real_loss_sum, self.d_loss_sum])
+        self.g_sum = tf.summary.merge([self.z_sum, self.D_fake_sum, self.G_sum, self.g_loss_sum])
+        self.d_sum = tf.summary.merge([self.z_sum, self.D_real_sum, self.d_loss_sum])
         self.merged = tf.summary.merge_all()
         self.writer = tf.summary.FileWriter('./model/', self.s.graph)
