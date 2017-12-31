1b01c302dc285661a7176d5fc3220a20f4a80f81
kozistr <kozistr@gmail.com>
2017-12-31 16:36:55 +0900

    add initial files (copied from BGAN)

--- AdaGAN/adagan_model.py
+++ AdaGAN/adagan_model.py
@@ -1,1 +1,183 @@
-# initial file
+import tensorflow as tf
+
+
+tf.set_random_seed(777)  # reproducibility
+
+
+def conv2d(x, f=64, k=4, d=2, pad='SAME', name='conv2d'):
+    """
+    :param x: input
+    :param f: filters, default 64
+    :param k: kernel size, default 3
+    :param d: strides, default 2
+    :param pad: padding (valid or same), default same
+    :param name: scope name, default conv2d
+    :return: covn2d net
+    """
+    return tf.layers.conv2d(x,
+                            filters=f, kernel_size=k, strides=d,
+                            kernel_initializer=tf.contrib.layers.variance_scaling_initializer(),
+                            kernel_regularizer=tf.contrib.layers.l2_regularizer(5e-4),
+                            bias_initializer=tf.zeros_initializer(),
+                            padding=pad, name=name)
+
+
+def deconv2d(x, f=64, k=4, d=2, pad='SAME', name='deconv2d'):
+    """
+    :param x: input
+    :param f: filters, default 64
+    :param k: kernel size, default 3
+    :param d: strides, default 2
+    :param pad: padding (valid or same), default same
+    :param name: scope name, default deconv2d
+    :return: decovn2d net
+    """
+    return tf.layers.conv2d_transpose(x,
+                                      filters=f, kernel_size=k, strides=d,
+                                      kernel_initializer=tf.contrib.layers.variance_scaling_initializer(),
+                                      kernel_regularizer=tf.contrib.layers.l2_regularizer(5e-4),
+                                      bias_initializer=tf.zeros_initializer(),
+                                      padding=pad, name=name)
+
+
+def batch_norm(x, momentum=0.9, eps=1e-9):
+    return tf.layers.batch_normalization(inputs=x,
+                                         momentum=momentum,
+                                         epsilon=eps,
+                                         scale=True,
+                                         training=True)
+
+
+class AdaGAN:
+
+    def __init__(self, s, batch_size=64, input_height=28, input_width=28, input_channel=1, n_classes=10,
+                 sample_num=64, sample_size=8, output_height=28, output_width=28,
+                 n_input=784, fc_unit=256,
+                 z_dim=100, g_lr=1e-4, d_lr=1e-4, epsilon=1e-9):
+
+        """
+        # General Settings
+        :param s: TF Session
+        :param batch_size: training batch size, default 32
+        :param input_height: input image height, default 28
+        :param input_width: input image width, default 28
+        :param input_channel: input image channel, default 1 (gray-scale)
+        - in case of MNIST, image size is 28x28x1(HWC).
+        :param n_classes: input dataset's classes
+        - in case of MNIST, 10 (0 ~ 9)
+
+        # Output Settings
+        :param sample_num: the number of output images, default 64
+        :param sample_size: sample image size, default 8
+        :param output_height: output images height, default 28
+        :param output_width: output images width, default 28
+
+        # For DNN model
+        :param n_input: input image size, default 784(28x28)
+        :param fc_unit: fully connected units, default 256
+
+        # Training Option
+        :param z_dim: z dimension (kinda noise), default 100
+        :param g_lr: generator learning rate, default 1e-3
+        :param d_lr: discriminator learning rate, default 1e-3
+        :param epsilon: epsilon, default 1e-9
+        """
+
+        self.s = s
+        self.batch_size = batch_size
+
+        self.input_height = input_height
+        self.input_width = input_width
+        self.input_channel = input_channel
+        self.image_shape = [self.batch_size, self.input_height, self.input_width, self.input_channel]
+        self.n_classes = n_classes
+
+        self.sample_num = sample_num
+        self.sample_size = sample_size
+        self.output_height = output_height
+        self.output_width = output_width
+
+        self.n_input = n_input
+        self.fc_unit = fc_unit
+
+        self.z_dim = z_dim
+        self.beta1 = 0.5
+        self.beta2 = 0.95
+        self.d_lr, self.g_lr = d_lr, g_lr
+        self.eps = epsilon
+
+        self.d_loss = 0.
+        self.g_loss = 0.
+
+        # Placeholder
+        self.x = tf.placeholder(tf.float32, shape=[None, self.n_input], name="x-image")  # (-1, 784)
+        self.z = tf.placeholder(tf.float32, shape=[None, self.z_dim], name='z-noise')    # (-1, 100)
+
+        self.build_adagan()  # build AdaGAN model
+
+    def discriminator(self, x, reuse=None):
+        with tf.variable_scope("discriminator", reuse=reuse):
+            for i in range(2):
+                x = tf.layers.dense(x, units=self.fc_unit, name='d-fc-%d' % i)
+                x = tf.nn.leaky_relu(x)
+
+            logits = tf.layers.dense(x, units=1, name='d-fc-2')
+            prob = tf.nn.sigmoid(logits)
+
+        return prob, logits
+
+    def generator(self, x, reuse=None):
+        with tf.variable_scope("generator", reuse=reuse):
+            for i in range(2):
+                x = tf.layers.dense(x, units=self.fc_unit, name='g-fc-%d' % i)
+                x = batch_norm(x)
+                x = tf.nn.leaky_relu(x)
+
+            logits = tf.layers.dense(x, units=self.n_input, name='g-fc-2')
+            prob = tf.nn.sigmoid(logits)
+
+        return prob
+
+    def build_adagan(self):
+        def log(x):
+            return tf.log(x + self.eps)
+
+        # Generator
+        self.g = self.generator(self.z)
+
+        # Discriminator
+        d_real, _ = self.discriminator(self.x)
+        d_fake, _ = self.discriminator(self.g, reuse=True)
+
+        # Losses
+        d_real_loss = -tf.reduce_mean(log(d_real))
+        d_fake_loss = -tf.reduce_mean(log(1. - d_fake))
+        self.d_loss = d_real_loss + d_fake_loss
+        self.g_loss = tf.reduce_mean(tf.square(log(d_fake) + log(1. - d_fake))) / 2
+
+        # Summary
+        tf.summary.histogram("z-noise", self.z)
+
+        g = tf.reshape(self.g, shape=self.image_shape)
+        tf.summary.image("generated", g)  # generated images by Generative Model
+        tf.summary.scalar("d_real_loss", d_real_loss)
+        tf.summary.scalar("d_fake_loss", d_fake_loss)
+        tf.summary.scalar("d_loss", self.d_loss)
+        tf.summary.scalar("g_loss", self.g_loss)
+
+        # Optimizer
+        vars = tf.trainable_variables()
+        d_params = [v for v in vars if v.name.startswith('d')]
+        g_params = [v for v in vars if v.name.startswith('g')]
+
+        self.d_op = tf.train.AdamOptimizer(learning_rate=self.d_lr,
+                                           beta1=self.beta1, beta2=self.beta2).minimize(self.d_loss, var_list=d_params)
+        self.g_op = tf.train.AdamOptimizer(learning_rate=self.g_lr,
+                                           beta1=self.beta1, beta2=self.beta2).minimize(self.g_loss, var_list=g_params)
+
+        # Merge summary
+        self.merged = tf.summary.merge_all()
+
+        # Model saver
+        self.saver = tf.train.Saver(max_to_keep=1)
+        self.writer = tf.summary.FileWriter('./model/', self.s.graph)
---
+++ AdaGAN/readme.md
@@ -0,0 +1,15 @@
+# Boosting Generative Adversarial Models
+
+## Loss Function
+
+
+## Architecture Networks
+
+
+## Tensorboard Result
+
+![result](https://github.com/kozistr/Awesome-GANs/blob/master/AdaGAN/adagan_tb.png)
+
+## To-Do
+* Add tensorboard result
+* Add Loss Function & Explains
