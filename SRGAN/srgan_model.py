184cd3c2c33c53a871bc49c17356c3f575881b4a
kozistr <kozistr@gmail.com>
2018-01-02 14:15:49 +0900

    add implementation of D net

--- SRGAN/srgan_model.py
+++ SRGAN/srgan_model.py
@@ -1,17 +1,16 @@
 import tensorflow as tf
-import numpy as np
 
 
 tf.set_random_seed(777)  # reproducibility
 
 
-def conv2d(x, f=64, k=3, d=1, act=tf.nn.elu, pad='SAME', name='conv2d'):
+def conv2d(x, f=64, k=3, d=1, act=None, pad='SAME', name='conv2d'):
     """
     :param x: input
     :param f: filters, default 64
     :param k: kernel size, default 3
     :param d: strides, default 2
-    :param act: activation function, default elu
+    :param act: activation function, default None
     :param pad: padding (valid or same), default same
     :param name: scope name, default conv2d
     :return: covn2d net
@@ -25,25 +24,29 @@ def conv2d(x, f=64, k=3, d=1, act=tf.nn.elu, pad='SAME', name='conv2d'):
                             padding=pad, name=name)
 
 
-def resize_nn(x, size):
-    return tf.image.resize_nearest_neighbor(x, size=(int(size), int(size)))
+def batch_norm(x, momentum=0.9, eps=1e-9):
+    return tf.layers.batch_normalization(inputs=x,
+                                         momentum=momentum,
+                                         epsilon=eps,
+                                         scale=True,
+                                         training=True)
 
 
 class SRGAN:
 
-    def __init__(self, s, batch_size=64, input_height=64, input_width=64, input_channel=3,
+    def __init__(self, s, batch_size=16, input_height=32, input_width=32, input_channel=3,
                  sample_num=1, sample_size=64, output_height=64, output_width=64,
                  df_dim=64, gf_dim=64,
-                 z_dim=128, g_lr=1e-4, d_lr=1e-4, epsilon=1e-12):
+                 z_dim=128, g_lr=1e-4, d_lr=1e-4):
 
         """
         # General Settings
         :param s: TF Session
-        :param batch_size: training batch size, default 64
+        :param batch_size: training batch size, default 16
         :param input_height: input image height, default 32
         :param input_width: input image width, default 32
         :param input_channel: input image channel, default 3 (RGB)
-        - in case of Celeb-A, image size is 64x64x3(HWC).
+        - in case of Celeb-A, image size is 32x32x3(HWC).
 
         # Output Settings
         :param sample_num: the number of output images, default 1
@@ -59,7 +62,6 @@ class SRGAN:
         :param z_dim: z dimension (kinda noise), default 128
         :param g_lr: generator learning rate, default 1e-4
         :param d_lr: discriminator learning rate, default 1e-4
-        :param epsilon: epsilon, default 1e-12
         """
 
         self.s = s
@@ -79,16 +81,28 @@ class SRGAN:
         self.gf_dim = gf_dim
 
         self.z_dim = z_dim
-        self.beta1 = 0.5
+        self.beta1 = 0.9
         self.beta2 = 0.999
         self.d_lr = d_lr
         self.g_lr = g_lr
-        self.eps = epsilon
+        self.lr_decay_rate = 0.5
+        self.lr_low_boundary = 1e-5
 
+        # LR update
+        self.d_lr_update = tf.assign(self.d_lr, tf.maximum(self.d_lr * self.lr_decay_rate, self.lr_low_boundary))
+        self.g_lr_update = tf.assign(self.g_lr, tf.maximum(self.g_lr * self.lr_decay_rate, self.lr_low_boundary))
+
+        # pre-defined
         self.d_real = 0
         self.d_fake = 0
         self.d_loss = 0.
         self.g_loss = 0.
+
+        self.d_op = None
+        self.g_op = None
+        self.merged = None
+        self.writer = None
+        self.saver = None
 
         # Placeholders
         self.x = tf.placeholder(tf.float32, shape=self.image_shape, name="x-image")  # (-1, 64, 64, 3)
@@ -96,65 +110,27 @@ class SRGAN:
 
         self.build_began()  # build BEGAN model
 
-    def encoder(self, x, reuse=None):
-        """
-        :param x: images
-        :param reuse: re-usable
-        :return: embeddings
-        """
-        with tf.variable_scope('encoder', reuse=reuse):
-            repeat = int(np.log2(self.input_height)) - 2
-
-            x = conv2d(x, f=self.df_dim, name="enc-conv-0")
-
-            for i in range(1, repeat + 1):
-                f = self.df_dim * i
-
-                x = conv2d(x, f=f, name="enc-conv-%d" % (i * 2 - 1))
-                x = conv2d(x, f=f, name="enc-conv-%d" % (i * 2))
-
-                if i < repeat:
-                    # x = tf.layers.max_pooling2d(x, 2, 2)
-                    # x = conv2d(x, f=f, d=2)  # conv pooling
-                    x = tf.layers.average_pooling2d(x, 2, 2, padding='SAME', name="enc-subsample-%d" % i)
-
-            x = tf.layers.flatten(x)
-            x = tf.layers.dense(x, units=self.z_dim * 8 * 8, name='enc-fc-1')
-
-            return x
-
-    def decoder(self, x, reuse=None):
-        """
-        :param x: embeddings
-        :param reuse: re-usable
-        :return: logits
-        """
-        with tf.variable_scope('decoder', reuse=reuse):
-            repeat = int(np.log2(self.input_height)) - 2
-
-            # x = tf.layers.dense(x, units=self.z_dim * 8 * 8, activation=tf.nn.elu, name='dec-fc-1')
-            x = tf.reshape(x, [self.batch_size, 8, 8, self.z_dim])
-
-            for i in range(1, repeat + 1):
-                x = conv2d(x, f=self.gf_dim, name="dec-conv-%d" % (i * 2 - 1))
-                x = conv2d(x, f=self.gf_dim, name="dec-conv-%d" % (i * 2))
-
-                if i < repeat:
-                    x = resize_nn(x, x.get_shape().as_list()[1] * 2)  # NN up-sampling
-
-            x = conv2d(x, 3)
-
-            return x
-
     def discriminator(self, x, reuse=None):
         """
-        :param x: images
-        :param reuse: re-usable
-        :return: logits
+        :param x: images (-1, 32, 32, 3)
+        :param reuse: re-usability
+        :return: prob
         """
         with tf.variable_scope("discriminator", reuse=reuse):
-            x = self.encoder(x, reuse=reuse)
-            x = self.decoder(x, reuse=reuse)
+            x = conv2d(x, self.df_dim, act=tf.nn.leaky_relu, name='d-conv2d-0')
+
+            filters = [1, 2, 2, 4, 4, 8, 8]
+            filters = list(map(lambda fs: fs * self.df_dim, filters))
+
+            for i, f in enumerate(filters):
+                x = conv2d(x, f=f, k=3, d=i % 2, act=tf.nn.leaky_relu, name='d-conv2d-%d' % (i + 1))
+                x = batch_norm(x)
+
+            # x : (-1, 4, 4, 8 * 64)
+            x = tf.layers.flatten(x)  # (-1, 4 * 4 * 8 * 64)
+
+            x = tf.layers.dense(x, 1024, activation=tf.nn.leaky_relu, name='d-fc-0')
+            x = tf.layers.dense(x, 1, activation=tf.nn.sigmoid, name='d-fc-1')
 
             return x
 
@@ -165,21 +141,9 @@ class SRGAN:
         :return: logits
         """
         with tf.variable_scope("generator", reuse=reuse):
-            repeat = int(np.log2(self.input_height)) - 2
+            # z : (batch_size, 128)
 
-            x = tf.layers.dense(z, units=self.z_dim * 8 * 8, activation=tf.nn.elu, name='g-fc-1')
-            x = tf.reshape(x, [self.batch_size, 8, 8, self.z_dim])
-
-            for i in range(1, repeat + 1):
-                x = conv2d(x, f=self.gf_dim, name="g-conv-%d" % (i * 2 - 1))
-                x = conv2d(x, f=self.gf_dim, name="g-conv-%d" % (i * 2))
-
-                if i < repeat:
-                    x = resize_nn(x, x.get_shape().as_list()[1] * 2)  # NN up-sampling
-
-            x = conv2d(x, 3)
-
-            return x
+            return z
 
     def build_began(self):
         def mse_loss(pred, data):
@@ -208,9 +172,9 @@ class SRGAN:
         tf.summary.scalar("g_loss", self.g_loss)
 
         # Optimizer
-        vars = tf.trainable_variables()
-        d_params = [v for v in vars if v.name.startswith('d')]
-        g_params = [v for v in vars if v.name.startswith('g')]
+        t_vars = tf.trainable_variables()
+        d_params = [v for v in t_vars if v.name.startswith('d')]
+        g_params = [v for v in t_vars if v.name.startswith('g')]
 
         self.d_op = tf.train.AdamOptimizer(learning_rate=self.d_lr,
                                            beta1=self.beta1, beta2=self.beta2).minimize(self.d_loss, var_list=d_params)
