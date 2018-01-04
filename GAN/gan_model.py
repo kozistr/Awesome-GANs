d365e8136b86ddeabce077398ee693f3d11fbe41
kozistr <kozistr@gmail.com>
2018-01-04 12:21:02 +0900

    update GAN D/G nets

--- GAN/gan_model.py
+++ GAN/gan_model.py
@@ -7,9 +7,9 @@ tf.set_random_seed(777)  # reproducibility
 class GAN:
 
     def __init__(self, s, batch_size=32, input_height=28, input_width=28, channel=1, n_classes=10,
-                 sample_num=64, sample_size=8, output_height=28, output_width=28,
-                 n_input=784, n_hidden_layer_1=128,
-                 z_dim=100, g_lr=8e-4, d_lr=8e-4, epsilon=1e-9):
+                 sample_num=10 * 10, sample_size=10, output_height=28, output_width=28,
+                 n_input=784, fc_unit=128,
+                 z_dim=128, g_lr=8e-4, d_lr=8e-4, epsilon=1e-9):
 
         """
         # General Settings
@@ -30,10 +30,10 @@ class GAN:
 
         # For DNN model
         :param n_input: input image size, default 784(28x28)
-        :param n_hidden_layer_1: first NN hidden layer, default 128
+        :param fc_unit: fully connected units, default 128
 
         # Training Option
-        :param z_dim: z dimension (kinda noise), default 100
+        :param z_dim: z dimension (kinda noise), default 128
         :param g_lr: generator learning rate, default 8e-4
         :param d_lr: discriminator learning rate, default 8e-4
         :param epsilon: epsilon, default 1e-9
@@ -53,57 +53,12 @@ class GAN:
         self.output_width = output_width
 
         self.n_input = n_input
-        self.n_hl_1 = n_hidden_layer_1
+        self.fc_unit = fc_unit
 
         self.z_dim = z_dim
+        self.beta1 = 0.5
         self.d_lr, self.g_lr = d_lr, g_lr
         self.eps = epsilon
-
-        ''' Weights
-
-        - discriminator
-        (784, 128) -> (128, 10)
-        
-        - generator
-        (100, 128) -> (128, 784)
-        
-        Initializer : HE initializer
-        '''
-        self.W = {
-            # for discriminator
-            'd_h1': tf.get_variable('d_h1',
-                                    shape=[self.n_input, self.n_hl_1],
-                                    initializer=tf.contrib.layers.variance_scaling_initializer()),
-            'd_h_out': tf.get_variable('d_h_out',
-                                       shape=[self.n_hl_1, 1],
-                                       initializer=tf.contrib.layers.variance_scaling_initializer()),
-            # for generator
-            'g_h1': tf.get_variable('g_h1',
-                                    shape=[self.z_dim, self.n_hl_1],
-                                    initializer=tf.contrib.layers.variance_scaling_initializer()),
-            'g_h_out': tf.get_variable('g_h_out',
-                                       shape=[self.n_hl_1, self.n_input],
-                                       initializer=tf.contrib.layers.variance_scaling_initializer()),
-        }
-
-        ''' Biases
-        
-        - discriminator
-        (128), (1)
-        
-        - generator
-        (128), (784)
-        
-        Initializer : zero initializer
-        '''
-        self.b = {
-            # for discriminator
-            'd_b1': tf.Variable(tf.zeros([self.n_hl_1])),
-            'd_b_out': tf.Variable(tf.zeros([1])),
-            # for generator
-            'g_b1': tf.Variable(tf.zeros([self.n_hl_1])),
-            'g_b_out': tf.Variable(tf.zeros([self.n_input])),
-        }
 
         '''
         self.batch = tf.Variable(0)
@@ -115,6 +70,16 @@ class GAN:
             staircase=True
         )
         '''
+        # pre-defined
+        self.d_loss = 0.
+        self.g_loss = 0.
+
+        self.d_op = None
+        self.g_op = None
+
+        self.merged = None
+        self.writer = None
+        self.saver = None
 
         # Placeholder
         self.x = tf.placeholder(tf.float32, shape=[None, self.n_input], name="x-image")  # (-1, 784)
@@ -124,23 +89,19 @@ class GAN:
 
     def discriminator(self, x, reuse=None):
         with tf.variable_scope("discriminator", reuse=reuse):
-            net = tf.nn.bias_add(tf.matmul(x, self.W['d_h1']), self.b['d_b1'])
-            net = tf.nn.leaky_relu(net)
+            x = tf.layers.flatten(x)
 
-            logits = tf.nn.bias_add(tf.matmul(net, self.W['d_h_out']), self.b['d_b_out'])
-            # prob = tf.nn.sigmoid(logits)
+            x = tf.layers.dense(x, self.fc_unit, activation=tf.nn.leaky_relu, name='d-fc-1')
+            x = tf.layers.dense(x, 1, activation=None, name='d-fc-2')
 
-        return logits
+        return x
 
     def generator(self, z, reuse=None):
         with tf.variable_scope("generator", reuse=reuse):
-            de_net = tf.nn.bias_add(tf.matmul(z, self.W['g_h1']), self.b['g_b1'])
-            de_net = tf.nn.leaky_relu(de_net)
+            x = tf.layers.dense(z, self.fc_unit, activation=tf.nn.leaky_relu, name='g-fc-1')
+            x = tf.layers.dense(x, self.n_input, activation=tf.nn.sigmoid, name='g-fc-2')
 
-            logits = tf.nn.bias_add(tf.matmul(de_net, self.W['g_h_out']), self.b['g_b_out'])
-            prob = tf.nn.sigmoid(logits)
-
-        return prob
+        return x
 
     def build_gan(self):
         # Generator
@@ -152,9 +113,6 @@ class GAN:
 
         # General GAN loss function referred in the paper
         """
-        # Maximize log(D(G(z)))
-        # Maximize log(D(x)) + log(1 - D(G(z)))
-
         log = lambda x: tf.log(x + self.eps)
 
         self.g_loss = -tf.reduce_mean(log(d_fake))
@@ -175,28 +133,28 @@ class GAN:
         self.d_loss = tf.reduce_sum(d_real / b_plus) + tf.log(z_b)
 
         # Summary
-        z_sum = tf.summary.histogram("z-noise", self.z)
+        tf.summary.histogram("z-noise", self.z)
 
-        self.g = tf.reshape(self.g, shape=[-1, self.output_height, self.output_height, self.channel])
-        g_sum = tf.summary.image("generated", self.g)  # generated images by Generative Model
-        d_real_sum = tf.summary.histogram("d_real", d_real)
-        d_fake_sum = tf.summary.histogram("d_fake", d_fake)
-        d_loss_sum = tf.summary.scalar("d_loss", self.d_loss)
-        g_loss_sum = tf.summary.scalar("g_loss", self.g_loss)
+        # g = tf.reshape(self.g, shape=[-1, self.output_height, self.output_height, self.channel])
+        # tf.summary.image("generated", g)  # generated images by Generative Model
+        tf.summary.histogram("d_real", d_real)
+        tf.summary.histogram("d_fake", d_fake)
+        tf.summary.scalar("d_loss", self.d_loss)
+        tf.summary.scalar("g_loss", self.g_loss)
 
-        # model saver
-        self.saver = tf.train.Saver()
+        # Optimizer
+        t_vars = tf.trainable_variables()
+        d_params = [v for v in t_vars if v.name.startswith('d')]
+        g_params = [v for v in t_vars if v.name.startswith('g')]
 
-        # optimizer
-        vars = tf.trainable_variables()
-        d_params = [v for v in vars if v.name.startswith('d_')]
-        g_params = [v for v in vars if v.name.startswith('g_')]
+        self.d_op = tf.train.AdamOptimizer(learning_rate=self.d_lr,
+                                           beta1=self.beta1).minimize(self.d_loss, var_list=d_params)
+        self.g_op = tf.train.AdamOptimizer(learning_rate=self.g_lr,
+                                           beta1=self.beta1).minimize(self.g_loss, var_list=g_params)
 
-        self.d_op = tf.train.AdamOptimizer(self.d_lr).minimize(self.d_loss, var_list=d_params)
-        self.g_op = tf.train.AdamOptimizer(self.g_lr).minimize(self.g_loss, var_list=g_params)
-
-        # merge summary
-        self.g_sum = tf.summary.merge([z_sum, d_fake_sum, g_sum, g_loss_sum])
-        self.d_sum = tf.summary.merge([z_sum, d_real_sum, d_loss_sum])
+        # Merge summary
         self.merged = tf.summary.merge_all()
+
+        # Model saver
+        self.saver = tf.train.Saver(max_to_keep=1)
         self.writer = tf.summary.FileWriter('./model/', self.s.graph)
--- GAN/gan_train.py
+++ GAN/gan_train.py
@@ -23,8 +23,7 @@ results = {
 
 train_step = {
     'global_step': 250001,
-    'logging_interval': 25000,
-    'update_overpowered': 10,
+    'logging_interval': 2500,
 }
 
 
@@ -48,84 +47,70 @@ def main():
         sample_x, _ = mnist.train.next_batch(model.sample_num)
         sample_z = np.random.uniform(-1., 1., [model.sample_num, model.z_dim]).astype(np.float32)
 
-        # original sample image
-        # original_image_height = model.sample_size
-        # original_image_width = model.sample_size
-        # original_dir = results['output'] + 'original.png'
-
-        # original image save
-        # original_x = sample_x.reshape([-1, model.input_height, model.input_width, model.channel])
-        # iu.save_images(original_x,
-        #                size=[original_image_height, original_image_width],
-        #                image_path=original_dir)
-
         d_overpowered = False
         for step in range(train_step['global_step']):
-            batch_x, _ = mnist.train.next_batch(model.batch_size)  # with batch_size, 32
-            batch_x = batch_x.reshape(-1, model.n_input)           # (-1, 784)
-
-            # Generate z
-            batch_z = np.random.uniform(-1., 1.,  # range -1 ~ 1
-                                        size=[model.batch_size, model.z_dim]).astype(np.float32)  # 32 x 128
+            batch_x, _ = mnist.train.next_batch(model.batch_size)
+            batch_x = batch_x.reshape(-1, model.n_input)
+            batch_z = np.random.uniform(-1., 1., size=[model.batch_size, model.z_dim]).astype(np.float32)
 
             # Update D network
             if not d_overpowered:
                 _, d_loss = s.run([model.d_op, model.d_loss],
                                   feed_dict={
                                       model.x: batch_x,
-                                      model.z: batch_z
+                                      model.z: batch_z,
                                   })
 
             # Update G network
             _, g_loss = s.run([model.g_op, model.g_loss],
                               feed_dict={
                                   model.x: batch_x,
-                                  model.z: batch_z
+                                  model.z: batch_z,
                               })
 
-            # Logging
-            if step % train_step['update_overpowered'] == 0:
+            d_overpowered = d_loss < (g_loss / 2)
+
+            if step % train_step['logging_interval'] == 0:
+                batch_x, _ = mnist.test.next_batch(model.batch_size)
+                batch_z = np.random.uniform(-1., 1., [model.batch_size, model.z_dim]).astype(np.float32)
+
+                d_loss, g_loss, summary = s.run([model.d_loss, model.g_loss, model.merged],
+                                                feed_dict={
+                                                    model.x: batch_x,
+                                                    model.z: batch_z,
+                                                })
+
                 d_overpowered = d_loss < (g_loss / 2)
 
-                if step % train_step['logging_interval'] == 0:
-                    batch_x, _ = mnist.test.next_batch(model.batch_size)
-                    batch_z = np.random.uniform(-1., 1., [model.batch_size, model.z_dim]).astype(np.float32)
+                # Print loss
+                print("[+] Step %08d => " % step,
+                      " D loss : {:.8f}".format(d_loss),
+                      " G loss : {:.8f}".format(g_loss))
 
-                    d_loss, g_loss, summary = s.run([model.d_loss, model.g_loss, model.merged],
-                                                    feed_dict={
-                                                        model.x: batch_x,
-                                                        model.z: batch_z
-                                                    })
+                # Training G model with sample image and noise
+                samples = s.run(model.g,
+                                feed_dict={
+                                    model.x: sample_x,
+                                    model.z: sample_z,
+                                })
 
-                    d_overpowered = d_loss < (g_loss / 2)
+                samples = np.reshape(samples, [-1, model.output_height, model.output_width, model.channel])
 
-                    # Print loss
-                    print("[+] Step %08d => " % step,
-                          "D loss : {:.8f}".format(d_loss), " G loss : {:.8f}".format(g_loss),
-                          "Overpowered :", d_overpowered)
+                # Summary saver
+                model.writer.add_summary(summary, step)
 
-                    # Training G model with sample image and noise
-                    samples = s.run(model.g,
-                                    feed_dict={
-                                        model.x: sample_x,
-                                        model.z: sample_z
-                                    })
+                # Export image generated by model G
+                sample_image_height = model.sample_size
+                sample_image_width = model.sample_size
+                sample_dir = results['output'] + 'train_{:08d}.png'.format(step)
 
-                    # Summary saver
-                    model.writer.add_summary(summary, step)
+                # Generated image save
+                iu.save_images(samples,
+                               size=[sample_image_height, sample_image_width],
+                               image_path=sample_dir)
 
-                    # Export image generated by model G
-                    sample_image_height = model.sample_size
-                    sample_image_width = model.sample_size
-                    sample_dir = results['output'] + 'train_{:08d}.png'.format(step)
-
-                    # Generated image save
-                    iu.save_images(samples,
-                                   size=[sample_image_height, sample_image_width],
-                                   image_path=sample_dir)
-
-                    # Model save
-                    model.saver.save(s, results['model'], global_step=step)
+                # Model save
+                model.saver.save(s, results['model'], global_step=step)
 
     end_time = time.time() - start_time  # Clocking end
 
