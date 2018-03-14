a182a4b486d1d375a8a33cc3277ba6dd1a80a1ad
kozistr <kozistr@gmail.com>
2018-03-14 13:56:40 +0900

    change DataSet Loader

--- WGAN/wgan_train.py
+++ WGAN/wgan_train.py
@@ -5,8 +5,6 @@ from __future__ import division
 import tensorflow as tf
 import numpy as np
 
-from tensorflow.examples.tutorials.mnist import input_data
-
 import sys
 import time
 
@@ -14,6 +12,8 @@ import wgan_model as wgan
 
 sys.path.append('../')
 import image_utils as iu
+from datasets import MNISTDataSet as DataSet
+
 
 results = {
     'output': './gen_img/',
@@ -31,7 +31,7 @@ def main():
     start_time = time.time()  # Clocking start
 
     # MNIST Dataset load
-    mnist = input_data.read_data_sets('./MNIST_data', one_hot=True)
+    mnist = DataSet().data
 
     # GPU configure
     config = tf.ConfigProto()
@@ -40,7 +40,7 @@ def main():
     with tf.Session(config=config) as s:
         # WGAN Model
         model = wgan.WGAN(s,
-                          enable_bn=False,
+                          enable_bn=True,
                           enable_adam=True,
                           enable_gp=True)  # Improved-WGAN with gradient penalty
 
