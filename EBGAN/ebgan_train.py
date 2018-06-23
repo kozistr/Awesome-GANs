a157e55ed106540594129414a08e84494cf2cc38
kozistr <kozistr@gmail.com>
2018-06-24 04:39:46 +0900

    change: g_test to g

--- EBGAN/ebgan_train.py
+++ EBGAN/ebgan_train.py
@@ -99,7 +99,7 @@ def main():
 
                     # Training G model with sample image and noise
                     sample_z = np.random.uniform(-1., 1., [model.sample_num, model.z_dim]).astype(np.float32)
-                    samples = s.run(model.g_test,
+                    samples = s.run(model.g,
                                     feed_dict={
                                         model.z: sample_z,
                                     })
