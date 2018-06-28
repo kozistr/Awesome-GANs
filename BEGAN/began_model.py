b85aebd084bd7b7cf6c18c8265443718582fcf1c
kozistr <kozistr@gmail.com>
2018-06-29 03:19:14 +0900

    add: skip connection at Dec/G nets & codes stuff...

--- BEGAN/began_model.py
+++ BEGAN/began_model.py
@@ -12,8 +12,8 @@ tf.set_random_seed(777)  # reproducibility
 class BEGAN:
 
     def __init__(self, s, batch_size=16, height=64, width=64, channel=3,
-                 sample_num=8 * 8, sample_size=8,
-                 df_dim=64, gf_dim=64, gamma=0.5, lambda_k=1e-3, z_dim=256, g_lr=2e-4, d_lr=2e-4):
+                 sample_num=10 * 10, sample_size=10,
+                 df_dim=64, gf_dim=64, gamma=0.5, lambda_k=1e-3, z_dim=128, g_lr=2e-4, d_lr=2e-4):
 
         """
         # General Settings
@@ -25,17 +25,17 @@ class BEGAN:
         - in case of Celeb-A, image size is 32x32x3/64x64x3(HWC).
 
         # Output Settings
-        :param sample_num: the number of output images, default 64
-        :param sample_size: sample image size, default 64
+        :param sample_num: the number of output images, default 100
+        :param sample_size: sample image size, default 10
 
         # For CNN model
         :param df_dim: discriminator filter, default 64
         :param gf_dim: generator filter, default 64
 
         # Training Option
-        :param gamma: gamma value, default 0.4
+        :param gamma: gamma value, default 0.5
         :param lambda_k: lr adjustment value lambda k, default 1e-3
-        :param z_dim: z dimension (kinda noise), default 256
+        :param z_dim: z dimension (kinda noise), default 128
         :param g_lr: generator learning rate, default 1e-4
         :param d_lr: discriminator learning rate, default 1e-4
         """
@@ -73,7 +73,6 @@ class BEGAN:
         self.balance = 0.
 
         self.g = None
-        self.g_test = None
 
         self.k_update = None
         self.d_op = None
@@ -92,7 +91,7 @@ class BEGAN:
 
         # Placeholders
         self.x = tf.placeholder(tf.float32, shape=[None, self.height, self.width, self.channel],
-                                name="x-image")  # (-1, 32 or 64, 32 or 64, 3)
+                                name="x-image")  # (-1, 64, 64, 3)
         self.z = tf.placeholder(tf.float32, shape=[None, self.z_dim],
                                 name='z-noise')  # (-1, 128)
 
@@ -158,6 +157,7 @@ class BEGAN:
                     x = t.up_sampling(x, tf.image.ResizeMethod.NEAREST_NEIGHBOR)  # NN up-sampling
 
             x = t.conv2d(x, f=self.channel)
+            x = tf.nn.tanh(x)
             return x
 
     def discriminator(self, x, reuse=None):
@@ -185,11 +185,16 @@ class BEGAN:
 
             x = tf.reshape(x, [-1, 8, 8, self.z_dim])
 
+            shortcut = tf.identity(x, name='shortcut')
+
             for i in range(1, repeat + 1):
                 x = t.conv2d(x, f=self.gf_dim, name="gen-conv2d-%d" % (i * 2 - 1))
                 x = tf.nn.elu(x)
                 x = t.conv2d(x, f=self.gf_dim, name="gen-conv2d-%d" % (i * 2))
                 x = tf.nn.elu(x)
+
+                if i < 3:
+                    x = tf.add(x, shortcut)
 
                 if i < repeat:
                     x = t.up_sampling(x, tf.image.ResizeMethod.NEAREST_NEIGHBOR)  # NN up-sampling
