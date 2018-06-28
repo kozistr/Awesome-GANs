84ac45b812a2ccdb980e8aa280e3ec61c2abc429
kozistr <kozistr@gmail.com>
2018-06-29 02:20:00 +0900

    update: initial AdaGAN implementation

--- AdaGAN/adagan_model.py
+++ AdaGAN/adagan_model.py
@@ -1,76 +1,32 @@
 import tensorflow as tf
 
+import sys
+
+sys.path.append('../')
+import tfutil as t
+
 
 tf.set_random_seed(777)  # reproducibility
 
 
-def conv2d(x, f=16, k=5, d=2, pad='SAME', name='conv2d'):
-    """
-    :param x: input
-    :param f: filters, default 64
-    :param k: kernel size, default 3
-    :param d: strides, default 2
-    :param pad: padding (valid or same), default same
-    :param name: scope name, default conv2d
-    :return: covn2d net
-    """
-    return tf.layers.conv2d(x,
-                            filters=f, kernel_size=k, strides=d,
-                            kernel_initializer=tf.contrib.layers.variance_scaling_initializer(),
-                            kernel_regularizer=tf.contrib.layers.l2_regularizer(5e-4),
-                            bias_initializer=tf.zeros_initializer(),
-                            padding=pad, name=name)
-
-
-def deconv2d(x, f=16, k=5, d=2, pad='SAME', name='deconv2d'):
-    """
-    :param x: input
-    :param f: filters, default 64
-    :param k: kernel size, default 3
-    :param d: strides, default 2
-    :param pad: padding (valid or same), default same
-    :param name: scope name, default deconv2d
-    :return: decovn2d net
-    """
-    return tf.layers.conv2d_transpose(x,
-                                      filters=f, kernel_size=k, strides=d,
-                                      kernel_initializer=tf.contrib.layers.variance_scaling_initializer(),
-                                      kernel_regularizer=tf.contrib.layers.l2_regularizer(5e-4),
-                                      bias_initializer=tf.zeros_initializer(),
-                                      padding=pad, name=name)
-
-
-def batch_norm(x, momentum=0.9, eps=1e-9):
-    return tf.layers.batch_normalization(inputs=x,
-                                         momentum=momentum,
-                                         epsilon=eps,
-                                         scale=True,
-                                         training=True)
-
-
 class AdaGAN:
 
-    def __init__(self, s, batch_size=64, input_height=28, input_width=28, input_channel=1, n_classes=10,
-                 sample_num=64, sample_size=8, output_height=28, output_width=28,
-                 n_input=784, df_dim=16, gf_dim=16, fc_unit=256,
-                 z_dim=100, g_lr=5e-3, d_lr=1e-3, c_lr=1e-4, epsilon=1e-9):
+    def __init__(self, s, batch_size=64, height=28, width=28, channel=1, n_classes=10,
+                 sample_num=64, sample_size=8,
+                 n_input=784, df_dim=16, gf_dim=16, fc_unit=256, z_dim=100, d_lr=1e-3, g_lr=5e-3, c_lr=1e-4):
 
         """
         # General Settings
         :param s: TF Session
         :param batch_size: training batch size, default 32
-        :param input_height: input image height, default 28
-        :param input_width: input image width, default 28
-        :param input_channel: input image channel, default 1 (gray-scale)
-        - in case of MNIST, image size is 28x28x1(HWC).
-        :param n_classes: input dataset's classes
-        - in case of MNIST, 10 (0 ~ 9)
+        :param height: input image height, default 28
+        :param width: input image width, default 28
+        :param channel: input image channel, default 1 (gray-scale)
+        :param n_classes: input DataSet's classes
 
         # Output Settings
         :param sample_num: the number of output images, default 64
         :param sample_size: sample image size, default 8
-        :param output_height: output images height, default 28
-        :param output_width: output images width, default 28
 
         # Hyper Parameters
         :param n_input: input image size, default 784(28x28)
@@ -80,25 +36,22 @@ class AdaGAN:
 
         # Training Option
         :param z_dim: z dimension (kinda noise), default 100
-        :param g_lr: generator learning rate, default 5e-3
         :param d_lr: discriminator learning rate, default 1e-3
+        :param g_lr: generator learning rate, default 5e-3
         :param c_lr: classifier learning rate, default 1e-4
-        :param epsilon: epsilon, default 1e-9
         """
 
         self.s = s
         self.batch_size = batch_size
 
-        self.input_height = input_height
-        self.input_width = input_width
-        self.input_channel = input_channel
-        self.image_shape = [self.batch_size, self.input_height, self.input_width, self.input_channel]
+        self.height = height
+        self.width = width
+        self.channel = channel
+        self.image_shape = [self.batch_size, self.height, self.width, self.channel]
         self.n_classes = n_classes
 
         self.sample_num = sample_num
         self.sample_size = sample_size
-        self.output_height = output_height
-        self.output_width = output_width
 
         self.n_input = n_input
         self.df_dim = df_dim
@@ -110,11 +63,20 @@ class AdaGAN:
         self.d_lr = d_lr
         self.g_lr = g_lr
         self.c_lr = c_lr
-        self.eps = epsilon
 
         self.d_loss = 0.
         self.g_loss = 0.
         self.c_loss = 0.
+
+        self.g = None
+
+        self.d_op = None
+        self.g_op = None
+        self.c_op = None
+
+        self.merged = None
+        self.saver = None
+        self.writer = None
 
         # Placeholder
         self.x = tf.placeholder(tf.float32, shape=[None, self.n_input], name="x-image")  # (-1, 784)
@@ -129,37 +91,34 @@ class AdaGAN:
     def discriminator(self, x, reuse=None):
         with tf.variable_scope("discriminator", reuse=reuse):
             for i in range(1, 3):
-                x = conv2d(x, self.df_dim * i, name='d-conv2d-%d' % i)
-                x = batch_norm(x)
+                x = t.conv2d(x, self.df_dim * i, 5, 2, name='disc-conv2d-%d' % i)
+                x = t.batch_norm(x, name='disc-bn-%d' % i)
                 x = tf.nn.leaky_relu(x, alpha=0.3)
 
-            logits = tf.layers.dense(x, units=1, name='d-fc-1')
+            x = t.flatten(x)
+
+            logits = t.dense(x, 1, name='disc-fc-1')
             prob = tf.nn.sigmoid(logits)
+            return prob, logits
 
-        return prob, logits
-
-    def generator(self, z, reuse=None):
+    def generator(self, z, reuse=None, is_train=True):
         with tf.variable_scope("generator", reuse=reuse):
-            x = tf.layers.dense(z, self.gf_dim * 7 * 7, name='g-fc-1')
-            x = batch_norm(x)
+            x = t.dense(z, self.gf_dim * 7 * 7, name='gen-fc-1')
+            x = t.batch_norm(x, name='gen-bn-1')
             x = tf.nn.leaky_relu(x, alpha=0.3)
 
-            x = tf.reshape(x, [self.batch_size, 7, 7, self.gf_dim])
+            x = tf.reshape(x, [-1, 7, 7, self.gf_dim])
 
             for i in range(1, 3):
-                x = deconv2d(x, self.gf_dim, name='g-fc-%d' % i)
-                x = batch_norm(x)
-                x = tf.nn.leaky_relu(x)
+                x = t.deconv2d(x, self.gf_dim, 5, 2, name='gen-deconv2d-%d' % (i + 1))
+                x = t.batch_norm(x, is_train=is_train, name='gen-bn-%d' % (i + 1))
+                x = tf.nn.leaky_relu(x, alpha=0.3)
 
-            logits = deconv2d(x, f=1, d=1, name='g-deconv-3')
-            prob = tf.nn.sigmoid(logits)
-
-        return prob
+            x = t.deconv2d(x, 1, 5, 1, name='gen-deconv2d-3')
+            x = tf.nn.sigmoid(x)
+            return x
 
     def build_adagan(self):
-        def log(x):
-            return tf.log(x + self.eps)
-
         # Generator
         self.g = self.generator(self.z)
 
@@ -168,27 +127,23 @@ class AdaGAN:
         d_fake, _ = self.discriminator(self.g, reuse=True)
 
         # Losses
-        d_real_loss = -tf.reduce_mean(log(d_real))
-        d_fake_loss = -tf.reduce_mean(log(1. - d_fake))  # proposed way : log(1. - d_fake) TO -log(d_fake)
+        d_real_loss = -tf.reduce_mean(t.safe_log(d_real))
+        d_fake_loss = -tf.reduce_mean(t.safe_log(1. - d_fake))
         self.d_loss = d_real_loss + d_fake_loss
-        self.g_loss = tf.reduce_mean(log(d_fake))
+        self.g_loss = tf.reduce_mean(t.safe_log(d_fake))
 
         # Summary
-        tf.summary.histogram("z-noise", self.z)
-
-        g = tf.reshape(self.g, shape=self.image_shape)
-        tf.summary.image("generated", g)  # generated images by Generative Model
-        tf.summary.scalar("d_real_loss", d_real_loss)
-        tf.summary.scalar("d_fake_loss", d_fake_loss)
-        tf.summary.scalar("d_loss", self.d_loss)
-        tf.summary.scalar("g_loss", self.g_loss)
-        tf.summary.scalar("c_loss", self.c_loss)
+        tf.summary.scalar("loss/d_real_loss", d_real_loss)
+        tf.summary.scalar("loss/d_fake_loss", d_fake_loss)
+        tf.summary.scalar("loss/d_loss", self.d_loss)
+        tf.summary.scalar("loss/g_loss", self.g_loss)
+        tf.summary.scalar("loss/c_loss", self.c_loss)
 
         # Optimizer
-        vars = tf.trainable_variables()
-        d_params = [v for v in vars if v.name.startswith('d')]
-        g_params = [v for v in vars if v.name.startswith('g')]
-        c_params = [v for v in vars if v.name.startswith('c')]
+        t_vars = tf.trainable_variables()
+        d_params = [v for v in t_vars if v.name.startswith('d')]
+        g_params = [v for v in t_vars if v.name.startswith('g')]
+        c_params = [v for v in t_vars if v.name.startswith('c')]
 
         self.d_op = tf.train.AdamOptimizer(learning_rate=self.d_lr,
                                            beta1=self.beta1).minimize(self.d_loss, var_list=d_params)
--- AdaGAN/readme.md
+++ AdaGAN/readme.md
@@ -2,14 +2,28 @@
 
 ## Loss Function
 
+* used ``GAN loss`` at D/G nets.
 
 ## Architecture Networks
 
+* Same with the AdaGAN paper.
+
+*DIFFS* | *AdaGAN Paper* | *ME*  |
+ :---:  |     :---:      | :---: |
+ **image size** | ``128`` | ``64`` |
+ **loss** | ``hinge loss`` | ``GAN loss`` |
 
 ## Tensorboard Result
 
-![result](https://github.com/kozistr/Awesome-GANs/blob/master/AdaGAN/adagan_tb.png)
+![result](./adagan_tb.png)
+
+> Elapsed Time : s with ``GTX 1060 6GB x 1``
+
+## Result
+
+*Name* | *Global Step 10k* | *Global Step 25k* | *Global Step 50k*
+:---: | :---: | :---: | :---:
+**AdaGAN**     | ![img](./gen_img/train_00010000.png) | ![img](./gen_img/train_00025000.png) | ![img](./gen_img/train_00050000.png)
 
 ## To-Do
-* Add tensorboard result
-* Add Loss Function & Explains
+* 
