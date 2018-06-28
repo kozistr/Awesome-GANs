50ed4639a4ea4f5a7d55f561a2e21e682c3d78c4
kozistr <kozistr@gmail.com>
2018-06-28 21:02:58 +0900

    fix: conv2d/deconv2d param name (filter)

--- tfutil.py
+++ tfutil.py
@@ -143,7 +143,7 @@ def conv2d_alt(x, f=64, k=3, s=1, pad=0, pad_type='zero', use_bias=True, sn=Fals
         if sn:
             w = tf.get_variable('kernel', shape=[k, k, x.get_shape()[-1], f],
                                 initializer=w_init, regularizer=w_reg)
-            x = tf.nn.conv2d(x, filters=spectral_norm(w), strides=[1, s, s, 1], padding='VALID')
+            x = tf.nn.conv2d(x, filter=spectral_norm(w), strides=[1, s, s, 1], padding='VALID')
 
             if use_bias:
                 b = tf.get_variable('bias', shape=[f], initializer=b_init)
@@ -219,7 +219,7 @@ def deconv2d_alt(x, f=64, k=3, s=1, use_bias=True, sn=False, name='deconv2d'):
         if sn:
             w = tf.get_variable('kernel', shape=[k, k, x.get_shape()[-1], f],
                                 initializer=w_init, regularizer=w_reg)
-            x = tf.nn.conv2d_transpose(x, filters=spectral_norm(w), strides=[1, s, s, 1], padding='SAME',
+            x = tf.nn.conv2d_transpose(x, filter=spectral_norm(w), strides=[1, s, s, 1], padding='SAME',
                                        output_shape=[x.get_shape()[0], x.get_shape()[1] * s, x.get_shape()[2] * s, f])
 
             if use_bias:
