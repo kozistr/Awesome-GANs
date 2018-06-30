73080f30c52ba54220accbeb627abada5dbf337e
kozistr <kozistr@gmail.com>
2018-06-30 15:13:13 +0900

    update: DIV2K low resolution image scaling to [-1,1]

--- datasets.py
+++ datasets.py
@@ -852,7 +852,7 @@ class Div2KDataSet:
                                        name=self.save_type,
                                        save_file_name=self.save_file_name + "-lr.h5",
                                        use_image_scaling=self.use_img_scaling,
-                                       image_scale='0,1',
+                                       image_scale='-1,1',
                                        img_save_method=cv2.INTER_CUBIC).raw_data  # numpy arrays
 
 
