a6c919860ad0f1cd7b2d4d8fd90b50f3cad99c6b
kozistr <kozistr@gmail.com>
2020-10-05 19:46:48 +0900

    refactor: bgan

--- awesome_gans/BGAN/bgan_train.py
+++ awesome_gans/BGAN/bgan_train.py
@@ -1,19 +1,11 @@
-from __future__ import absolute_import
-from __future__ import print_function
-from __future__ import division
-
-import tensorflow as tf
-import numpy as np
-
-import sys
 import time
 
-import bgan_model as bgan
+import numpy as np
+import tensorflow as tf
 
-sys.path.append('../')
-import image_utils as iu
-from datasets import MNISTDataSet as DataSet
-
+import awesome_gans.bgan.bgan_model as bgan
+import awesome_gans.image_utils as iu
+from awesome_gans.datasets import MNISTDataSet as DataSet
 
 results = {
     'output': './gen_img/',
