ce5211073576255ff5b1391867bc2d64b0136746
kozistr <kozistr@gmail.com>
2020-10-05 19:47:47 +0900

    refactor: wgan

--- awesome_gans/WGAN/wgan_train.py
+++ awesome_gans/WGAN/wgan_train.py
@@ -1,20 +1,12 @@
-from __future__ import absolute_import
-from __future__ import print_function
-from __future__ import division
-
-import tensorflow as tf
-import numpy as np
-
-import sys
 import time
 
-import wgan_model as wgan
+import numpy as np
+import tensorflow as tf
 
-sys.path.append('../')
-import image_utils as iu
-from datasets import DataIterator
-from datasets import CiFarDataSet as DataSet
-
+import awesome_gans.image_utils as iu
+import awesome_gans.wgan.wgan_model as wgan
+from awesome_gans.datasets import CiFarDataSet as DataSet
+from awesome_gans.datasets import DataIterator
 
 results = {
     'output': './gen_img/',
@@ -79,7 +71,7 @@ def main():
             print('[-] No checkpoint file found')
 
         global_step = saved_global_step
-        start_epoch = global_step // (len(ds.train_images) // model.batch_size)           # recover n_epoch
+        start_epoch = global_step // (len(ds.train_images) // model.batch_size)  # recover n_epoch
         ds_iter.pointer = saved_global_step % (len(ds.train_images) // model.batch_size)  # recover n_iter
         for epoch in range(start_epoch, train_step['epochs']):
             for _ in range(ds_iter.num_batches):
