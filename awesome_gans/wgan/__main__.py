2543e535c421e45ed9c843193e04558c53389587
kozistr <kozistr@gmail.com>
2020-10-12 10:30:44 +0900

    update: initialize()

--- awesome_gans/wgan/__main__.py
+++ awesome_gans/wgan/__main__.py
@@ -1,9 +1,12 @@
 from awesome_gans.config import parse_args
-from awesome_gans.utils import set_seed
+from awesome_gans.utils import initialize, set_seed
 
 
 def main():
     config = parse_args()
+
+    # initial tf settings
+    initialize()
 
     # reproducibility
     set_seed(config.seed)
