a8c394bd6795cdda2ce466b87c3528618442a8d1
kozistr <kozistr@gmail.com>
2020-10-12 11:58:35 +0900

    feature: RMSProp

--- awesome_gans/optimizers.py
+++ awesome_gans/optimizers.py
@@ -1,31 +1,16 @@
-from tensorflow.keras.optimizers import SGD, Adam
+from tensorflow.keras.optimizers import SGD, Adam, RMSprop
 
 
-def build_discriminator_optimizer(config):
-    optimizer_name: str = config.d_opt
-
+def build_optimizer(config, optimizer_name: str = 'adam'):
     if optimizer_name == 'adam':
         return Adam(
             learning_rate=config.d_lr,
             beta_1=config.beta1,
             beta_2=config.beta2,
         )
+    elif optimizer_name == 'rmsprop':
+        return RMSprop(learning_rate=config.d_lr)
     elif optimizer_name == 'sgd':
         return SGD(learning_rate=config.d_lr)
-    else:
-        raise NotImplementedError()
-
-
-def build_generator_optimizer(config):
-    optimizer_name: str = config.g_opt
-
-    if optimizer_name == 'adam':
-        return Adam(
-            learning_rate=config.g_lr,
-            beta_1=config.beta1,
-            beta_2=config.beta2,
-        )
-    elif optimizer_name == 'sgd':
-        return SGD(learning_rate=config.g_lr)
     else:
         raise NotImplementedError()
